ee7eb81652b17c78347c089372583120e8771d7b
javitocor <javitocor@gmail.com>
2020-05-01 05:05:46 +0200

    Generated users with devise

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, length: { minimum:5, maximum:40 }
+    validates :body, presence: true, length: { minimum:40, maximum:1500 }
 end
