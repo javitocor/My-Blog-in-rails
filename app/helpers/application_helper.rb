94847dd442072ad0cc1031691c117eb21a640ed6
javitocor <javitocor@gmail.com>
2020-05-04 06:07:53 +0200

    Added title helper to head title layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
             'gotham.jpg'
         end
     end
+    def full_title(page_title='')
+        base_title = "Gotham Blog"
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
