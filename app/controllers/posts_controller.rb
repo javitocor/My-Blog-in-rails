78fdcbdee886c8837894a4c6cf7f9894fba0705b
javitocor <javitocor@gmail.com>
2020-05-04 09:15:13 +0200

    Added rtag links to index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all.order('created_at DESC')
+    @tags = ActsAsTaggableOn::Tag.all
   end
 
   # GET /posts/1
